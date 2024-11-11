beb008ac76b23c357968340bd08263c221d36f62
samuele <samuelerovito002@gmail.com>
2024-11-11 10:49:32 +0100

    reso più snello il codice

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// Vertex Shader (in GLSL)
-const char* vertexShaderSource = R"(
-#version 330 core
-layout(location = 0) in vec3 aPos;
-void main()
-{
-    gl_Position = vec4(aPos, 1.0);
-})";
-
-// Fragment Shader (in GLSL)
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-void main()
-{
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
-})";
-
 int main() {
     // Inizializza GLFW
     if (!glfwInit()) {
@@ -28,7 +10,7 @@ int main() {
     }
 
     // Crea una finestra GLFW
-    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW OpenGL Example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "RadTrack", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -43,70 +25,6 @@ int main() {
         return -1;
     }
 
-    // Definisci i vertici per un triangolo
-    float vertices[] = {
-        0.0f,  0.5f, 0.0f,  // Top vertex
-       -0.5f, -0.5f, 0.0f,  // Bottom-left vertex
-        0.5f, -0.5f, 0.0f   // Bottom-right vertex
-    };
-
-    // Crea e compila il Vertex Shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-
-    // Controlla errori di compilazione
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // Crea e compila il Fragment Shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    // Controlla errori di compilazione
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // Crea un programma di shader
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Controlla errori di collegamento
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    // Pulisci gli shader, non sono più necessari dopo il linking
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Crea un VBO (Vertex Buffer Object) e un VAO (Vertex Array Object)
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    // Collega il VBO e il VAO
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
     // Loop principale
     while (!glfwWindowShouldClose(window)) {
         // Gestisci gli input
@@ -116,20 +34,9 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Usa il programma degli shader
-        glUseProgram(shaderProgram);
-
-        // Disegna il triangolo
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
         // Swap buffers
         glfwSwapBuffers(window);
     }
-
-    // Pulisci
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 
     glfwDestroyWindow(window);
     glfwTerminate();
